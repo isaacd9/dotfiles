bf7dd76c9051b27e290f308ef06d722af27f3130
Isaac Diamond <isaacd9@gmail.com>
2018-03-13 00:33:07 -0700

    Install script

--- install.sh
+++ install.sh
@@ -7,5 +7,7 @@ ln -sfh `pwd`/.gitignore_global ~/.gitignore_global
 ln -sfh `pwd`/.profile ~/.profile
 ln -sfh `pwd`/.tmux.conf ~/.tmux.conf
 ln -sfh `pwd`/.vimrc ~/.vimrc
+ln -sfh `pwd`/.nvimrc ~/.nvimrc
 ln -sfh `pwd`/.zshrc ~/.zshrc
+ln -sfh `pwd`/.config/nvim ~/.config/nvim
 
